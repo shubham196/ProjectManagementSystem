36ce0d5fa5ca535647ed43319e14326e3b2178fc
shubhampatel196@gmail.com <shubhampatel196@gmail.com>
2022-12-30 22:49:35 +0530

    all completed only task,project and user profile page remain

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,10 +10,14 @@ class Task extends Model
 {
     use HasFactory, FilterByTenant;
 
-    protected $fillable = ['name', 'project_id'];
+    protected $fillable = ['name', 'project_id','user_id','team_id','task_description','start_date','end_date','priority','status','files'];
 
     public function project()
     {
         return $this->belongsTo(Project::class);
+    }
+    public function users()
+    {
+        return $this->belongsToMany(User::class)->withPivot('is_owner');
     }
 }
