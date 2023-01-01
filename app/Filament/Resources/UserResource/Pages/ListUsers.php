f8b0fc92f736531f3e211ae0506f105cde1fe814
shubhampatel196@gmail.com <shubhampatel196@gmail.com>
2023-01-02 00:11:22 +0530

    Invitation current_tenant_id on null bug is fixed

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -10,10 +10,10 @@ class ListUsers extends ListRecords
 {
     protected static string $resource = UserResource::class;
 
-    // protected function getActions(): array
-    // {
-    //     // return [
-    //     //     Actions\CreateAction::make(),
-    //     // ];
-    // }
+    protected function getActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
-use App\Traits\FilterByTenant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Traits\HasRoles;
 class Invitation extends Model
 {
-    use HasFactory,FilterByTenant,HasRoles;
+    use HasFactory,HasRoles;
 
     protected $fillable = ['tenant_id', 'email', 'token', 'accepted_at'];
 
